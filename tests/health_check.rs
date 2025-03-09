5c3ce9b992568016950469649cb509d62a086532
Vitalie Brinza <231pithagora079@gmail.com>
2025-03-09 21:41:40 +0100

    refactor

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,23 @@
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-}
+use std::{fmt::format, mem::zeroed, net::TcpListener};
 
+fn spawn_app() -> String{
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let response = client 
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
