739607d76b123c1d1f2ee76b7da137bc42592de8
Vitalie Brinza <231pithagora079@gmail.com>
2025-03-18 10:12:33 +0100

    enable logger too

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::net::TcpListener;
 use sqlx::PgPool;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    LogTracer::init().expect("Failed to set logger.");
+
     let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
+        .unwrap_or(EnvFilter::new("info")
+    );
     let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
          std::io::stdout);
@@ -18,6 +22,7 @@ async fn main() -> Result<(), std::io::Error> {
         .with(JsonStorageLayer)
         .with(formatting_layer);
     set_global_default(subscriber).expect("Failed to set subscriber.");
+
     let configuration = get_configuration().expect("Failed to read configration.");
     let connection_pool = PgPool::connect(
         &configuration.database.connection_string()
@@ -26,5 +31,6 @@ async fn main() -> Result<(), std::io::Error> {
         .expect("Failed to connect to the Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
