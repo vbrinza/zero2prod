fe6dde0824017ce03a2c26858bfd146a41b41e43
Vitalie Brinza <231pithagora079@gmail.com>
2025-03-22 19:07:46 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to post
+
+```bash
+curl -i -X POST -d 'email=thomas_mann@hotmail.com&name=Tom' http://127.0.0.1:8000/subscriptions
+```
--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,10 @@
 use actix_web::{web, App, HttpServer};
-use actix_web::middleware::Logger;
 use std::net::TcpListener;
 use actix_web::dev::Server;
 use sqlx::PgPool;
 use crate::routes::health_check;
 use crate::routes::subscribe;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(
     listener: TcpListener,
@@ -13,7 +13,7 @@ pub fn run(
     let db_pool = web::Data::new(db_ppol);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
