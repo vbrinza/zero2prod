1545fb2b6bcb2a018bc359c8d61588814d13df0d
Vitalie Brinza <231pithagora079@gmail.com>
2025-03-27 22:38:55 +0100

    add name validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
-
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,7 +23,9 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-   
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish()
+    }
     match insert_subscriber(&pool, &form).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -59,4 +61,15 @@ pub async fn insert_subscriber(
         e
     })?;
     Ok(())
+}
+
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+    let is_too_long = s.graphemes(true).count() > 256;
+    let forbiden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s
+        .chars()
+        .any(|g| forbiden_characters.contains(&g));
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
