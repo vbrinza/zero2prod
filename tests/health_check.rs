3bb50d13968af21d29cb219d52964caa7282cb1e
Vitalie Brinza <231pithagora079@gmail.com>
2025-03-29 10:28:39 +0100

    add more tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,5 @@
 
 use secrecy::Secret;
-use secrecy::ExposeSecret;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use sqlx::{PgPool, PgConnection, Connection, Executor};
@@ -145,3 +144,30 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     }
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitively-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+            assert_eq!(
+                200,
+                response.status().as_u16(),
+                "The API did not return a 200 OK when the payload wass {}.",
+                description
+            )        
+    }
+}
