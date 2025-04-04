c375b52b26a1e0fa2b3192357329c52007b7bedb
Vitalie Brinza <231pithagora079@gmail.com>
2025-04-04 09:04:45 +0200

    made it work

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,7 @@ use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use sqlx::{PgPool, PgConnection, Connection, Executor};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-
+use zero2prod::email_client::EmailClient;
 use std:: net::TcpListener;
 use uuid::Uuid;
 use std::sync::LazyLock;
@@ -44,7 +44,13 @@ async fn spawn_app() -> TestApp{
         .expect("Failed to read configration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
-    let server = run(listener,connection_pool.clone()).expect("Failed to bind address");
+    let sender_email = configuration.email_client.sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url, 
+        sender_email
+    );
+    let server = run(listener,connection_pool.clone(), email_client).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     TestApp {
         address,
