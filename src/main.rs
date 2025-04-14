13b8e64425be27c1d53e9c14407143a99563d69c
Vitalie Brinza <231pithagora079@gmail.com>
2025-04-15 00:04:00 +0200

    refactored tests, main and run

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::build;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -15,19 +12,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configration.");
-    let connection_pool = PgPoolOptions::new()
-        .connect_lazy_with(configuration.database.connect_options());
-    let sender_email = configuration.email_client.sender()
-        .expect("Invalid sender email address.");
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-    );
-    let address = format!(
-        "{}:{}", 
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await?;
+    let server = build(configuration).await?;
+    server.await?;
     Ok(())
 }
